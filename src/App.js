486729825832e590ecf134cea08a574ef3d9bffc
Graham McLaughlin <grahammclaughlin1324@gmail.com>
2020-11-24 17:52:58 -0500

    Connected to UserService Properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+//Formatting
 import './App.css';
+import { Nav, Button } from 'react-bootstrap'
 
-function App() {
+//Functional
+import { createBrowserHistory } from "history";
+import React from 'react'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+import MicroFrontend from './MicroFrontend'
+
+
+const defaultHistory = createBrowserHistory();
+
+const {
+  REACT_APP_USERS_HOST: usersHost,
+  REACT_APP_TASKS_HOST: tasksHost,
+  REACT_APP_PAYMENT_HOST: paymentHost,
+  REACT_APP_SERVICE_REQUEST_HOST: serviceRequestHost
+} = process.env;
+
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Maintain Yo Self</h1>
+      <h4>An App to help organize your home maintainence</h4>
     </div>
   );
 }
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className="">
+        <Header />
+        <BrowserRouter>
+          <Link to={'/users'}>
+            Users
+          </Link>
+          <br></br>
+          <Link to={'/tasks'}>
+            Tasks
+          </Link>
+          <br></br>
+          <Link to={'/contractors'}>
+            Contractors
+          </Link>
+          <br></br>
+          <Link to={'/servicerequest'}>
+            Service Requests
+          </Link>
+          <br></br>
+          <Link to={'/payment'}>
+            Payment
+          </Link>
+          <Route path="/users" component={User} />
+          <Route path="/contractors" component={Contractor} />
+          <Route path="/tasks" component={Task} />
+          <Route path="/servicerequest" component={ServiceRequest} />
+          <Route path="/payment" component={Payment} />
+        </BrowserRouter>
+      Test
+      </div >
+    );
+  }
+}
+const User = ({history}) => (
+  <MicroFrontend history={history} host={usersHost} name="UserService" />
+)
+const Contractor = ({ history }) => (
+  <div>
+    Contractor Profile is displayed
+  </div>
+)
+const Task = ({ history }) => (
+  <div>
+    <MicroFrontend history={history} host={tasksHost} name="tasks" />
+  </div>
+)
+
+const ServiceRequest = ({ history }) => (
+  <div>
+    <MicroFrontend history={history} host={serviceRequestHost} name="ServiceRequest" />
+  </div>
+)
+
+const Payment = ({ history }) => (
+  <div>
+    <MicroFrontend history={history} host={paymentHost} name="Payment" />
+  </div>
+)
 
 export default App;
